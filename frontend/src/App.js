255b451b3ec1ecf6570ea21e79c95cd5fee52923
ajopek <arjopek@gmail.com>
2019-05-11 22:40:52 +0200

    Add first home page css skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppRouter  from './AppRouter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <AppRouter />
           <h>VOTE 7</h>
         </header>
       </div>
