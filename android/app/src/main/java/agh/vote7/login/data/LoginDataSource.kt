6106dae527a486f79238400e4153beb62301bba9
mdronski <dronskimichal@gmail.com>
2019-05-11 16:35:40 +0200

    add login functionality

--- android/app/src/main/java/agh/vote7/login/data/LoginDataSource.kt
+++ android/app/src/main/java/agh/vote7/login/data/LoginDataSource.kt
@@ -4,64 +4,104 @@ import agh.vote7.login.data.model.LoggedInUser
 import android.annotation.SuppressLint
 import android.os.AsyncTask
 import android.util.Log
+import com.google.gson.Gson
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
-import java.net.URL
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 class LoginDataSource {
     private val client = OkHttpClient()
-    private val url = "http://192.168.43.77:8080/api/users/register"
+    val gson = Gson()
+    private val loginUrl = "http://192.168.43.77:8080/users/login"
+    private val getUserDataUrl = "http://192.168.43.77:8080/users/me"
+    private val logoutUrl = "http://192.168.43.77:8080/users/logout"
     private val JSON = MediaType.parse("application/json; charset=utf-8")
+    private var token : String? = null
+    private var user : LoggedInUser? = null
 
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
-            val poster = PostLogin()
-            poster.execute()
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+            PostLogin().execute().get()
+            GetUserData().execute().get()
+            return Result.Success(user!!)
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
         }
     }
 
     fun logout() {
-        // TODO: revoke authentication
+        try {
+            GetLogout().execute()
+        }catch (e: Throwable){
+            e.printStackTrace()
+        }
     }
 
     @SuppressLint("StaticFieldLeak")
     inner class PostLogin : AsyncTask<Any, Any, Any>() {
-        override fun doInBackground(vararg params: Any?): Any {
+        override fun doInBackground(vararg params: Any?) {
+
             val json = JSONObject()
-
             json.put("email", "mail@gmail.com")
-            json.put("password", "heheszki")
-            json.put("name", "michal")
-            json.put("surname", "kowalski")
-
+            json.put("password", "qwertyi")
 
             val body = RequestBody.create(JSON, json.toString())
-            Log.e("dbg", json.toString())
             val request = Request.Builder()
-                .url(url)
+                .url(loginUrl)
                 .post(body)
                 .build()
-            print("\n\n\n\n\n\n")
+
             try {
                 val response = client.newCall(request).execute()
-                Log.e("dbg", response.toString())
+                val jsonToken = JSONObject(response.body()?.string()!!)
+                Log.e("dbg", jsonToken.getString("token"))
+                token = jsonToken.getString("token")
             }catch (e: Exception){
                 e.printStackTrace()
             }
-            return "xd"
         }
-
     }
 
+    @SuppressLint("StaticFieldLeak")
+    inner class GetUserData : AsyncTask<Any, Any, Any>() {
+        override fun doInBackground(vararg params: Any?){
 
+            val request = Request.Builder()
+                .url("$getUserDataUrl?token=$token")
+                .get()
+                .build()
+
+            try {
+                val response = client.newCall(request).execute()
+                var responseBody = response.body()?.string()!!
+                user = gson.fromJson(responseBody, LoggedInUser::class.java)
+            }catch (e: Exception){
+
+                e.printStackTrace()
+            }
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class GetLogout : AsyncTask<Any, Any, Any>() {
+        override fun doInBackground(vararg params: Any?){
+
+            val request = Request.Builder()
+                .url("$logoutUrl?token=$token")
+                .get()
+                .build()
+
+            try {
+                val response = client.newCall(request).execute()
+                Log.e("dbg", response.body()?.string()!!)
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 
 
 }
