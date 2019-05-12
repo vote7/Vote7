6e1c567876ac09a63d40ae3731944c7faec03df4
Piotr Janczyk <pjanczyk97@gmail.com>
2019-05-12 22:14:38 +0200

    Configure Retrofit

--- android/app/src/main/java/agh/vote7/data/RestApiProvider.kt
+++ android/app/src/main/java/agh/vote7/data/RestApiProvider.kt
@@ -1,14 +1,65 @@
 package agh.vote7.data
 
+import agh.vote7.BuildConfig
+import com.ihsanbal.logging.Level
+import com.ihsanbal.logging.LoggingInterceptor
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.internal.platform.Platform
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
+private const val BASE_URL = "http://vote7.serveo.net/"
+
 object RestApiProvider {
-    val restApi: RestApi by lazy {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://materia.serveo.net/")
+    var token: String? = "1>aUdtfsDmiD8WvGetI5yv"
+    val restApi: RestApi by lazy { restApi() }
+
+    private fun restApi(): RestApi =
+        retrofit()
+            .create()
+
+    private fun retrofit(): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .baseUrl(BASE_URL)
+            .client(httpClient())
             .build()
 
-        retrofit.create<RestApi>()
-    }
+    private fun httpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(tokenInterceptor())
+            .addInterceptor(loggingInterceptor())
+            .build()
+
+    private fun tokenInterceptor(): Interceptor =
+        Interceptor { chain ->
+            val request = chain.request()
+
+            val urlBuilder = request.url().newBuilder()
+            token?.let {
+                urlBuilder.addQueryParameter("token", it)
+            }
+            val modifiedUrl = urlBuilder.build()
+
+            val modifiedRequest = request
+                .newBuilder()
+                .url(modifiedUrl)
+                .build()
+
+            chain.proceed(modifiedRequest)
+        }
+
+    private fun loggingInterceptor(): Interceptor =
+        LoggingInterceptor.Builder()
+            .loggable(BuildConfig.DEBUG)
+            .setLevel(Level.BASIC)
+            .log(Platform.INFO)
+            .request("Request")
+            .response("Response")
+            .enableAndroidStudio_v3_LogsHack(true)
+            .build()
 }
