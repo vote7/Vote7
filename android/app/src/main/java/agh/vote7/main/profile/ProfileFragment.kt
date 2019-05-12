7374c3ccb128715ef9474e3f298940083978265f
Piotr Janczyk <pjanczyk97@gmail.com>
2019-05-12 14:35:18 +0200

    Improve layout of ProfileFragment

--- android/app/src/main/java/agh/vote7/main/profile/ProfileFragment.kt
+++ android/app/src/main/java/agh/vote7/main/profile/ProfileFragment.kt
@@ -6,12 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // TODO(pjanczyk): Get data from API
+        textViewName.text = "John Smith"
+        textViewEmail.text = "john@example.com"
     }
 }
