f70382003c5e81005ad0b6c1c156e3a8c314a048
Piotr Janczyk <pjanczyk97@gmail.com>
2019-05-12 22:15:54 +0200

    Implement more calls to REST API

--- android/app/src/main/java/agh/vote7/data/RestApi.kt
+++ android/app/src/main/java/agh/vote7/data/RestApi.kt
@@ -1,9 +1,15 @@
 package agh.vote7.data
 
+import agh.vote7.data.model.Group
+import agh.vote7.data.model.User
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RestApi {
     @GET("users/me")
-    fun getUser(): Deferred<User>
+    fun getCurrentUser(): Deferred<User>
+
+    @GET("users/{userId}/groups")
+    fun getUserGroups(@Path("userId") userId: String): Deferred<List<Group>>
 }
--- android/app/src/main/java/agh/vote7/data/model/User.kt
+++ android/app/src/main/java/agh/vote7/data/model/User.kt
@@ -1,6 +1,7 @@
-package agh.vote7.data
+package agh.vote7.data.model
 
 data class User(
+    val id: String,
     val email: String,
     val name: String,
     val surname: String
