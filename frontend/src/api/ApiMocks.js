6dc80a1c0de00257e169887c0bc72be0e31be31a
ajopek <arjopek@gmail.com>
2019-05-21 23:36:34 +0200

    Add api mocks for adding and editing polls

--- frontend/src/api/ApiMocks.js
+++ frontend/src/api/ApiMocks.js
@@ -1,35 +1,52 @@
+let pollId = 3
+let polls =  [
+  {
+    id: 1,
+    name: "History quiz"
+  },
+  {
+    id: 2,
+    name: "Should the UK leave the EU?"
+  },
+]
+let questions = [
+  {
+    content: "In which year did WW2 started?",
+    open: true,
+  },
+  {
+    content: "Who was the first president of the United States?",
+    open: true,
+  },
+  {
+    content: "Which leader murdered more people",
+    open: false,
+    answers: ["Adolf Hitler", "Joseph Stalin", "Mao Zedong"],
+  },
+]
 const ApiMocks = {
-  getPolls: async () => [
-    {
-      id: 1,
-      name: "History quiz",
-    },
-    {
-      id: 2,
-      name: "Should the UK leave the EU?",
-    },
-  ],
+  getPolls: async () => polls,
 
   getPoll: async pollId => ({
     id: 1,
     name: "History quiz",
   }),
 
-  getQuestions: async pollId => [
-    {
-      content: "In which year did WW2 started?",
-      open: true,
-    },
-    {
-      content: "Who was the first president of the United States?",
-      open: true,
-    },
-    {
-      content: "Which leader murdered more people",
-      open: false,
-      answers: ["Adolf Hitler", "Joseph Stalin", "Mao Zedong"],
-    },
-  ],
+  getQuestions: async pollId => questions,
+
+  addPolls: async poll => {
+    poll["id"] = pollId;
+    polls.push(poll)
+    pollId = pollId + 1
+  },
+
+  addQuestion: async question => {
+      questions.push(question)
+  },
+
+  editPollName: async (pollId, newName) => {
+    polls = polls.map(({id, name}) => id === pollId ? {id: id, name: newName} : {id: id, name: name})
+  }
 };
 
 export default ApiMocks;
