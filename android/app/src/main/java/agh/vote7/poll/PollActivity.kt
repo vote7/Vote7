689f94f39a39c3aa38ba852af6b48ba07c133b37
Piotr Janczyk <pjanczyk97@gmail.com>
2019-05-27 20:44:20 +0200

    Rename VotingActivity to PollActivity

--- android/app/src/main/java/agh/vote7/poll/PollActivity.kt
+++ android/app/src/main/java/agh/vote7/poll/PollActivity.kt
@@ -1,14 +1,14 @@
-package agh.vote7.voting
+package agh.vote7.poll
 
 import agh.vote7.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_voting.*
+import kotlinx.android.synthetic.main.activity_poll.*
 
-class VotingActivity : AppCompatActivity() {
+class PollActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_voting)
+        setContentView(R.layout.activity_poll)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
